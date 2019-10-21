716291bb8faaf44955fb4eb0c11f23d4dcb1df7d
Harrish Suhumar <harrish.s@hotmail.ca>
2019-10-21 19:51:15 +0000

    show number of products and categories in admin dash

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
-  #http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV["user"] , password: ENV["pass"]
   def show
+    @count_products = Product.count
+    @count_cats = Category.count
   end
 end
